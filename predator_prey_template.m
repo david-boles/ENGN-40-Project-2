eff3330480fdd2005aa38bbe826c5a176679491e
Charles Shi <shicharles.107@gmail.com>
2019-03-14 14:28:10 -0400

    change to basic mind game strat

--- predator_prey_template.m
+++ predator_prey_template.m
@@ -16,7 +16,7 @@ function predator_prey_template
    initial_w = [150,1000,0,1000,0,0,0,0]; % Initial position/velocity
    force_table_predator = rand(51,2)-0.5;
    force_table_prey = rand(51,2)-0.5;
-   options = odeset('Events',@event,'RelTol',0.15);
+   options = odeset('Events',@event,'RelTol',0.01);
    [time_vals,sol_vals] = ode45(@(t,w) eom(t,w,mr,my,Frmax,Fymax,c,force_table_predator,force_table_prey), ...
        [0:1:250],initial_w,options);
    animate_projectiles(time_vals,sol_vals);
@@ -70,7 +70,7 @@ function [event,stop,direction] = event(t,w)
 % the ground as well.  See the MATLAB manual for how to detect and 
 % distinguish between multiple events if you want to do this 
     event = 1;
-    if (norm(pr - py) - 1 == 0) || (py(2) < 0) || (pr(2) < 0)
+    if (norm(pr - py) - 5 == 0) || (py(2) < 0) || (pr(2) < 0)
       event = 0;
       
     end
@@ -103,15 +103,100 @@ end
 %      magnitude you specify exceeds the maximum allowable
 %      value its magnitude will be reduced to this value 
 %      (without changing direction)
+function d2ydx2=bicubic_start_acceleration(Xs, Ys, Ms, Xe, Ye, Me)
+        h = Ms * (Xe - Xs);
+        i = Ye - Ys;
+        k = Me * (Xe - Xs);
+        b = 3*i - 2*h - k;
+        d2ydx2 = (2*b)/((Xe - Xs)^2);
+end
+function a=compute_acceleration_for_target(pS, vS, pE, vE, time)
+        Ax = bicubic_start_acceleration(0, pS(1), vS(1), time, pE(1), vE(1));
+        Ay = bicubic_start_acceleration(0, pS(2), vS(2), time, pE(2), vE(2));
+        a = [Ax; Ay];
+end
 
+    persistent lastTr lastVy lastAy;
 if (amiapredator)
     % Code to compute the force to be applied to the predator
     
+    
+     ay = 0;
+    if ~isempty(lastTr)
+        if lastTr ~= t
+            ay = (vy - lastVy)/(t - lastTr);
+        else
+            ay = lastAy;
+        end
+    end
+    %{
+    disp(t);
+    disp(lastTr);
+    disp(vy);
+    disp(lastVy);
+    disp(ay);
+    %}
+    
+    if sqrt((pr(1) - py(1))^2 +(pr(2) - py(2))^2) < 0
+        ar_required = @(t_int) compute_acceleration_for_target(pr, vr, py, vy * max(1, 1/(t_int ^ 2)), t_int);
+        ar_required_mag = @(t_int) norm(ar_required(t_int));
+        fr_required = @(t_int) (ar_required(t_int) - [0;-9.81]) * 100;
+        fr_required_mag = @(t_int) norm(fr_required(t_int));
+        t_int_best = 250;
+        t_search_max = 250;
+    else
+        py_expt = @(time) (1/2) * ay * min(time, 5)^2 + vy * min(time,20) + py;
+        ar_required = @(t_int) (2 * (py_expt(t_int) - (vr * t_int) - pr)) / (t_int ^ 2);
+        ar_required_mag = @(t_int) norm(ar_required(t_int));
+        fr_required = @(t_int) (ar_required(t_int) - [0;-9.81]) * 100;
+        fr_required_mag = @(t_int) norm(fr_required(t_int));
+        t_int_best = 10;
+        t_search_max = 10;
+    end
+    
+    for t_int = [0.1 : 0.1 : t_search_max]
+        if fr_required_mag(t_int) < (1.3 * 100 * 9.81)
+            t_int_best = t_int;
+            break;
+        end
+    end    
+    F = fr_required(t_int_best);
+    if norm(F) > (1.3 * 100 * 9.8) 
+        %disp("NOPE! Shortening...");
+        F = Frmax*F ./ norm(F);
+    end
+    
+    
+    min_gnd_dist = 1;
+    ar_max_y = Frmax/100-9.81-0.2*9.81;
+    vr_y = vr(2);
+    pr_y = pr(2);
+    
+    traject_min = -0.5*vr_y^2/ar_max_y + pr_y;
+    if traject_min < min_gnd_dist
+        F = [0;Frmax];    
+    end 
+    
+    if pr_y <= 0
+        disp(['PREDATOR GROUND COLLISION @y=', num2str(pr_y)]);
+    end 
+    
+    
+    if floor(t) > floor(lastTr)
+        disp(t);
+    end
+    lastTr = t;
+    lastVy = vy;
+    lastAy = ay;
+    %disp(F);
+    
+
     %dt = 2;
-    %vrel = vy-vr;
+   % vrel = vy-vr;
     %F = Frmax*(py-pr+dt*vrel)/norm(py-pr+dt*vrel);
     
     % DUSTIN's code
+    %{
     g = 9.81;
     mr = 100; % Mass of predator, in kg
     my = 10.; % Mass of prey, in kg
@@ -135,7 +220,7 @@ if (amiapredator)
             F = [x*Fr(1);Fn];
         end
     end
-
+    %}
 
 else
     % Code to compute the force to be applied to the prey
@@ -143,7 +228,7 @@ else
     
     
     % When it reaches below 100m
-    if py(2) < 100
+    if py(2) < 70
         % Goes straight back up
         F=Fymax*[0;1];
         
@@ -152,17 +237,35 @@ else
         rh = pr-py;
         rhmag = norm(rh);
         
-        prmag = norm(pr);
-        vrel = vy-vy;
+        %prmag = norm(pr);
+        vrel = vy-vr;
         vrmag = norm(vr);
-        vymag = norm(vy);
-        if (rhmag<40) || ...    
+        %vymag = norm(vy);
+        if (rhmag<40)
+            Fy = -(vr(1)/(vrmag+1.e-08));
+            Fx = (vr(2)/(vrmag+1.e-08));
+            if (vrel(1)*Fx+vrel(2)*Fy<0)
+                Fy=-Fy;
+                Fx=-Fx;
+            end
+
+        %{
+        elseif (rhmag<50) || ...    
            ((abs(vy(1)/norm(vy)-vr(1)/norm(vr))) < 0.1) && ...
            ((abs(vy(2)/norm(vy) - vr(2)/norm(vr))) < 0.1)
+
            % if predator speeds in our direction, make a sharp turn
+           %{
            C=1;
             %Fy = -C*pr(1)/(prmag+1.e-08)-C*(vr(1)/(vrmag+1.e-08));
             %Fx = C*pr(2)/(prmag+1.e-08)+C*(vr(2)/(vrmag+1.e-08));
+            Fy = -(vy(1)/(vymag+1.e-08));
+            Fx = (vy(2)/(vymag+1.e-08));
+            if (vrel(1)*Fx+vrel(2)*Fy<0)
+                Fy=-Fy;
+                Fx=-Fx;
+            end
+            %}
             
             % CASE 1: Predator on bottom left
             if (vr(1) > 15) && (vr(2) > 15)
@@ -174,67 +277,70 @@ else
                 end
                 %}
                 if vy(1) > 0
-                    Fy = (vy(1)/(vymag+1.e-08));
-                    Fx = -(vy(2)/(vymag+1.e-08));
+                    Fy = (vr(1)/(vrmag+1.e-08));
+                    Fx = -(vr(2)/(vrmag+1.e-08));
                 else
-                    Fy = -(vy(1)/(vymag+1.e-08));
-                    Fx = (vy(2)/(vymag+1.e-08));
+                    Fy = -(vr(1)/(vrmag+1.e-08));
+                    Fx = (vr(2)/(vrmag+1.e-08));
                 end
             % CASE 2: Predator on bottom right
             elseif (vr(1) < 15) && (vr(2) > 15)
                 if vy(1) > 0
-                    Fy = (vy(1)/(vymag+1.e-08));
-                    Fx = -(vy(2)/(vymag+1.e-08));
+                    Fy = (vr(1)/(vrmag+1.e-08));
+                    Fx = -(vr(2)/(vrmag+1.e-08));
                 else
-                    Fy = (vy(1)/(vymag+1.e-08));
-                    Fx = -(vy(2)/(vymag+1.e-08));
+                    Fy = (vr(1)/(vrmag+1.e-08));
+                    Fx = -(vr(2)/(vrmag+1.e-08));
                 end
                 
                 
             % CASE 3: Predator on top left
             elseif (vr(1) > 15) && (vr(2) < 15)
                 if vy(1) > 0
-                    Fy = (vy(1)/(vymag+1.e-08));
-                    Fx = -(vy(2)/(vymag+1.e-08));
+                    Fy = (vr(1)/(vrmag+1.e-08));
+                    Fx = -(vr(2)/(vrmag+1.e-08));
                 else
-                    Fy = (vy(1)/(vymag+1.e-08));
-                    Fx = (vy(2)/(vymag+1.e-08));
+                    Fy = (vr(1)/(vrmag+1.e-08));
+                    Fx = (vr(2)/(vrmag+1.e-08));
                 end
                 
             % CASE 4: Predator on top right
             elseif (vr(1) < 15) && (vr(2) < 15)
                 
                 if vy(1) > 0
-                    Fy = (vy(1)/(vymag+1.e-08));
-                    Fx = -(vy(2)/(vymag+1.e-08));
+                    Fy = (vr(1)/(vrmag+1.e-08));
+                    Fx = -(vr(2)/(vrmag+1.e-08));
                 else
-                    Fy = -(vy(1)/(vymag+1.e-08));
-                    Fx = (vy(2)/(vymag+1.e-08));
+                    Fy = -(vr(1)/(vrmag+1.e-08));
+                    Fx = (vr(2)/(vrmag+1.e-08));
                 end
             % OTHERWISE
             else
-                    Fy = -(vy(1)/(vymag+1.e-08));
-                    Fx = (vy(2)/(vymag+1.e-08));
+                    Fy = -(vr(1)/(vrmag+1.e-08));
+                    Fx = (vr(2)/(vrmag+1.e-08));
             end
-        
+            %}
+            
+        F = Fymax*[Fx;Fy]/norm([Fx;Fy]);
+        F = F + 2.*Fymax*[1;0]/py(2);
+        F = Fymax*F/norm(F);
         else
            % Sufficiently far apart
            
            
-           B = 2;
-           Fx = -(rh(1)+B*(vrel(1)));
-           Fy = -(rh(2)+B*(vrel(2)));
-          
-           
+           %B = 2;
+           %Fx = -(rh(1)+B*(vrel(1)));
+           %Fy = -(rh(2)+B*(vrel(2)));
+           %Fy = -1;
+           %Fx = -rh(1);
+           %Fy = -rh(2);
            % MIND GAMES APPROACH
-           %{
-            dt = 2;
-            F = Fymax*((py+dt*vy+(pr+dt*vr)))/norm(py+dt*vy-(pr+dt*vr));
-           %}
+           
+           dt = 4;
+           F = Fymax*((py+dt*vy+(pr+dt*vr)))/norm(py+dt*vy-(pr+dt*vr));
+   
         end
-        F = Fymax*[Fx;Fy]/norm([Fx;Fy]);
-        F = F + 2.*Fymax*[1;0]/py(2);
-        F = 0.5*Fymax*F/norm(F);
+        
     end
 end
 end
